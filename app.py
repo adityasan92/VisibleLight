6ca24f84c3c537fcbead1d08fabf09076ba09abc
adityasan92 <adityasan92@gmail.com>
2016-03-01 22:34:29 -0500

    The beginning.

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, jsonify
+from flask_restful import Resource, Api, reqparse
+import logging
+import sys
+
+app = Flask(__name__,  static_url_path='')
+api = Api(app)
+parser = reqparse.RequestParser()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
